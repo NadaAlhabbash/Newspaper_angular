8a2c9199dca0871ef9b0a9509e1ca6501d2fdc4f
NadaAlhabbash <nadaalhabbash@gmail.com>
2024-05-05 21:22:09 +0300

    css edits

---
+++ src/app/components/trends-news/trends-news.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trends-news',
+  templateUrl: './trends-news.component.html',
+  styleUrl: './trends-news.component.css'
+})
+export class TrendsNewsComponent {
+
+}
