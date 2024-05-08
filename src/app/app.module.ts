ea9700e77a83ac3e8ba02d5716409ea93a60457e
NadaAlhabbash <nadaalhabbash@gmail.com>
2024-05-08 10:59:49 +0300

    modify file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { SliderComponent } from './components/slider/slider.component';
-import { TrendsNewsComponent } from './components/trends-news/trends-news.component';
+import { HomeComponent } from './components/home/header/home.component';
+import { SliderComponent } from './components/home/slider/slider.component';
+import { TrendsNewsComponent } from './components/home/trends-news/trends-news.component';
 
 @NgModule({
   declarations: [
