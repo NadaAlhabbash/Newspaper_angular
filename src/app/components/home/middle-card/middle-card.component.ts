617b39cce5be888e7ef9048236b24a2d74b86910
NadaAlhabbash <nadaalhabbash@gmail.com>
2024-05-14 12:35:53 +0300

    adding the home section

---
+++ src/app/components/home/middle-card/middle-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-middle-card',
+  templateUrl: './middle-card.component.html',
+  styleUrl: './middle-card.component.css'
+})
+export class MiddleCardComponent {
+
+}
