077227fe4d69bcd2acee18076da1c53eba25c9f6
NadaAlhabbash <nadaalhabbash@gmail.com>
2024-05-15 15:53:49 +0300

    Resolved conflicts in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/header/home.component';
+import { HomeComponent } from './components/home/main_page/home.component';
 import { SliderComponent } from './components/home/slider/slider.component';
 import { TrendsNewsComponent } from './components/home/trends-news/trends-news.component';
+import { MiddleCardComponent } from './components/home/middle-card/middle-card.component';
+import { SiteSectionComponent } from './components/home/site-section/site-section.component';
+import { RecentNewsComponent } from './components/home/recent-news/recent-news.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SliderComponent,
-    TrendsNewsComponent,
+    TrendsNewsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home/site-section/site-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-section',
+  templateUrl: './site-section.component.html',
+  styleUrl: './site-section.component.css'
+})
+export class SiteSectionComponent {
+
+}
